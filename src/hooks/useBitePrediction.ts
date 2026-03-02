1099bd7dd4a2e9a08e0e5557c697b8f2f2cb5e7a
Sean McDonnell <seanebones@gmail.com>
2026-03-01 21:21:13 -0600

    Eleven: Update src/hooks/useBitePrediction.ts

--- src/hooks/useBitePrediction.ts
+++ src/hooks/useBitePrediction.ts
@@ -1,1 +1,17 @@
-/* Hooks */
+import * as tf from '@tensorflow/tfjs-react-native';
+import modelJSON from '../ml/biteModel.json';
+
+export const useBitePrediction = () => {
+  const [model, setModel] = useState(null);
+  const predict = async (features: number[]) => {
+    if (!model) {
+      await tf.ready();
+      const m = await tf.loadLayersModel(modelJSON);
+      setModel(m);
+    }
+    const input = tf.tensor2d([features]);
+    const output = model.predict(input) as tf.Tensor;
+    return (await output.data())[0];
+  };
+  return { predict };
+};
