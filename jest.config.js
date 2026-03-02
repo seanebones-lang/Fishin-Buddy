82cbd7420d3b938c334fb7c530dbffb9a02787f4
Sean McDonnell <seanebones@gmail.com>
2026-03-01 20:08:06 -0600

    Agent: Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,29 @@
-/* Full config above */
+module.exports = {
+  preset: 'jest-expo',
+  setupFilesAfterEnv: ['<rootDir>/__tests__/jest-setup.ts'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native(-community)?|expo|@expo|react-navigation|@react-navigation|@rnmapbox|@supabase/supabase-js|@lottie-react-native|react-native-svg|nativewind|reanimated)'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  testEnvironment: 'node',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    'src/**/*.tsx',
+    'App.tsx',
+    '!src/**/*.d.ts',
+    '!**/*.test.*',
+    '!**/node_modules/**'
+  ],
+  coverageReporters: ['text', 'lcov', 'html'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  moduleNameMapping: {
+    '^@/(.*)': '<rootDir>/src/$1'
+  }
+};
